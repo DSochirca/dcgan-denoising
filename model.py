cbf84e3b21c09f08fdbfc1e2485c9d8f02d07020
Dan Sochirca <dsochircaa@gmail.com>
2024-06-16 12:53:59 +0200

    Add code

--- model.py
+++ model.py
@@ -1,6 +1,56 @@
 import torch
 import torch.nn as nn
-import numpy as np
+from torch.nn.utils import spectral_norm
+class FactorizedSelfAttention(nn.Module):
+    def __init__(self, in_channels, embed_channels=None):
+        super(FactorizedSelfAttention, self).__init__()
+        if embed_channels is None:
+            embed_channels = in_channels // 8
+
+        self.C_embed = embed_channels
+
+        self.query_conv = nn.Conv2d(in_channels, embed_channels, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels, embed_channels, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        B, C, W, H = x.size()
+        C_embed = self.C_embed
+
+        # Project to query, key, value
+        # -> B, embed_channels, W, H
+        Q = self.query_conv(x)
+        K = self.key_conv(x)
+        V = self.value_conv(x)
+
+        # Reshape for row and column attention
+        Q_row = Q.permute(0, 2, 1, 3).contiguous().view(B * W, H, C_embed)  # (B, W, H, C_embed) -> (B*W, H, C_embed)
+        K_row = K.permute(0, 2, 1, 3).contiguous().view(B * W, H, C_embed)
+        V_row = V.permute(0, 2, 1, 3).contiguous().view(B * W, H, C)
+
+        # Row-wise attention
+        # A_row should be (B*W, H, H)
+        A_row = self.softmax(torch.bmm(Q_row, K_row.transpose(1, 2))) # (B*W, H, C_embed) x (B*W, C_embed, H) -> (B*W, H, H)
+        O_row = torch.bmm(A_row, V_row)  # (B*W, H, H) x (B*W, H, C) -> (B*W, H, C)
+        O_row = O_row.view(B, W, H, C).permute(0, 3, 1, 2)  # -> (B, C, W, H)
+
+        # Reshape for column attention
+        Q_col = O_row.permute(0, 3, 2, 1).contiguous().view(B * H, W, C) # (B, C, W, H) -> (B, H, W, C) -> (B*H, W, C)
+        K_col = O_row.permute(0, 3, 2, 1).contiguous().view(B * H, W, C)
+        V_col = O_row.permute(0, 3, 2, 1).contiguous().view(B * H, W, C)
+
+        # Column-wise attention
+        # A_col should be (B*H, W, W)
+        A_col = self.softmax(torch.bmm(Q_col, K_col.transpose(1, 2)))  # (B*H, W, C) x (B*H, C, W) -> (B*H, W, W)
+        O_col = torch.bmm(A_col, V_col)  # (B*H, W, W) x (B*H, W, C) -> (B*H, W, C)
+        O_col = O_col.view(B, H, W, C).permute(0, 3, 2, 1)  # -> (B, C, W, H)
+
+        # Combine with residual connection
+        y = self.gamma * O_col + x
+        return y
 
 class ResidualBlock(nn.Module):
     def __init__(self, num_channels, kernel_size=3, num_layers=2):
@@ -8,7 +58,7 @@ class ResidualBlock(nn.Module):
         layers = []
 
         for i in range(num_layers):
-            layers.append(nn.Conv2d(num_channels, num_channels, kernel_size=kernel_size, stride=1, padding=1))
+            layers.append(spectral_norm(nn.Conv2d(num_channels, num_channels, kernel_size=kernel_size, stride=1, padding=1)))
             layers.append(nn.BatchNorm2d(num_channels))
             layers.append(nn.ReLU())
 
@@ -46,18 +96,23 @@ class ResidualLayer(nn.Module):
         return y
 
 class Generator(nn.Module):
-    def __init__(self, in_channels, residual_channels=None, kernel_size=3, downsampling_factor=1):
+    def __init__(self, in_channels, residual_channels=None, kernel_size=3, downsampling_factor=1, uses_self_attention=False):
         super(Generator, self).__init__()
 
         if residual_channels is None:  residual_channels = [64, 64, 64, 64]
 
         self.downsampling_factor = downsampling_factor
+        self.uses_self_attention = uses_self_attention
 
-        self.conv1 = nn.Conv2d(in_channels, residual_channels[0], kernel_size=kernel_size, stride=1, padding=1)
+        self.conv1 = spectral_norm(nn.Conv2d(in_channels, residual_channels[0], kernel_size=kernel_size, stride=1, padding=1))
         self.relu1 = nn.ReLU()
 
         K = downsampling_factor
         assert K == 1 or K == 2 or K == 4 or K == 8
+
+        # Self-attention before 1st residual layer
+        if uses_self_attention:
+            self.attn1 = FactorizedSelfAttention(residual_channels[0], embed_channels=8)
 
         residual_layers = []
         for i in range(len(residual_channels)):
@@ -66,13 +121,17 @@ class Generator(nn.Module):
 
         self.residual_layers = nn.Sequential(*residual_layers)
 
-        self.conv2 = nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=kernel_size, stride=1, padding=1)
+        # Self-attention after last residual layer
+        if uses_self_attention:
+            self.attn2 = FactorizedSelfAttention(residual_channels[-1], embed_channels=8)
 
-        self.conv3 = nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=kernel_size, stride=1, padding=1)
+        self.conv2 = spectral_norm(nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=kernel_size, stride=1, padding=1))
+
+        self.conv3 = spectral_norm(nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=kernel_size, stride=1, padding=1))
         self.relu3 = nn.ReLU()
-        self.conv4 = nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=1, stride=1, padding=0)  # last 2 conv use kernel_size=1
+        self.conv4 = spectral_norm(nn.Conv2d(residual_channels[-1], residual_channels[-1], kernel_size=1, stride=1, padding=0))  # last 2 conv use kernel_size=1
         self.relu4 = nn.ReLU()
-        self.conv5 = nn.Conv2d(residual_channels[-1], in_channels, kernel_size=1, stride=1, padding=0)  # padding=0 if kernel_size=1
+        self.conv5 = spectral_norm(nn.Conv2d(residual_channels[-1], in_channels, kernel_size=1, stride=1, padding=0))  # padding=0 if kernel_size=1
 
         self.sigmoid = nn.Sigmoid()
 
@@ -80,8 +139,14 @@ class Generator(nn.Module):
         y = self.conv1(x)
         y = self.relu1(y)
 
+        if self.uses_self_attention:
+            y = self.attn1(y)
+
         residual = y
         y = self.residual_layers(y)
+
+        if self.uses_self_attention:
+            y = self.attn2(y)
 
         y = self.conv2(y)  # before the residual connection below, because it needs the same number of channels
 
@@ -96,32 +161,41 @@ class Generator(nn.Module):
         return y
 
 class Discriminator(nn.Module):
-    def __init__(self, input_size, in_channels, hidden_channels=None, kernel_size=3):
+    def __init__(self, input_size, in_channels, hidden_channels=None, kernel_size=3, uses_self_attention=False):
         super(Discriminator, self).__init__()
         output_size = input_size
 
         if hidden_channels is None:  hidden_channels = [64, 64, 128, 256, 512]  # an extra 64, as in the original model
+        self.uses_self_attention = uses_self_attention
 
-        self.conv1 = nn.Conv2d(in_channels, hidden_channels[0], kernel_size=kernel_size, stride=1, padding=1)
+        self.conv1 = spectral_norm(nn.Conv2d(in_channels, hidden_channels[0], kernel_size=kernel_size, stride=1, padding=1))
         self.lrelu1 = nn.LeakyReLU(0.2)
+
+        # Self-attention before 1st hidden layer
+        if uses_self_attention:
+            self.attn1 = FactorizedSelfAttention(hidden_channels[0], embed_channels=8)
 
         layers = []
         for i in range(1, len(hidden_channels)):
-            layers.append(nn.Conv2d(hidden_channels[i-1], hidden_channels[i], kernel_size=kernel_size, stride=2, padding=1))
+            layers.append(spectral_norm(nn.Conv2d(hidden_channels[i-1], hidden_channels[i], kernel_size=kernel_size, stride=2, padding=1)))
             layers.append(nn.BatchNorm2d(hidden_channels[i]))
             layers.append(nn.LeakyReLU(0.2))
 
         self.hidden_layers = nn.Sequential(*layers)
 
-        self.conv2 = nn.Conv2d(hidden_channels[-1], hidden_channels[-1], kernel_size=kernel_size, stride=1, padding=1)
+        # Self-attention after last hidden layer
+        if uses_self_attention:
+            self.attn2 = FactorizedSelfAttention(hidden_channels[-1], embed_channels=8)
+
+        self.conv2 = spectral_norm(nn.Conv2d(hidden_channels[-1], hidden_channels[-1], kernel_size=kernel_size, stride=1, padding=1))
         self.batch_norm2 = nn.BatchNorm2d(hidden_channels[-1])
         self.lrelu2 = nn.LeakyReLU(0.2)
 
-        self.conv3 = nn.Conv2d(hidden_channels[-1], hidden_channels[-1], kernel_size=1, stride=1, padding=0)
+        self.conv3 = spectral_norm(nn.Conv2d(hidden_channels[-1], hidden_channels[-1], kernel_size=1, stride=1, padding=0))
         self.batch_norm3 = nn.BatchNorm2d(hidden_channels[-1])
         self.lrelu3 = nn.LeakyReLU(0.2)
 
-        self.conv4 = nn.Conv2d(hidden_channels[-1], 1, kernel_size=1, stride=1, padding=0)
+        self.conv4 = spectral_norm(nn.Conv2d(hidden_channels[-1], 1, kernel_size=1, stride=1, padding=0))
 
         output_size = output_size // 4**(len(hidden_channels) - 1)  # (#hidden_channels - 1) layers. Power of 4 because we have 2d images (2*2) - output gets halved in each dimension
         output_size = output_size * 1  # 1 is the #channels in last conv layer
@@ -137,7 +211,13 @@ class Discriminator(nn.Module):
         y = self.conv1(x)
         y = self.lrelu1(y)
 
+        if self.uses_self_attention:
+            y = self.attn1(y)
+
         y = self.hidden_layers(y)
+
+        if self.uses_self_attention:
+            y = self.attn2(y)
 
         y = self.lrelu2(self.batch_norm2(self.conv2(y)))
         y = self.lrelu3(self.batch_norm3(self.conv3(y)))
@@ -151,64 +231,3 @@ class Discriminator(nn.Module):
         y = self.sigmoid(y)
 
         return y
-
-if __name__ == '__main__':
-    # Dummy input (1 image w/ 3 channels, 256x256)
-    np.random.seed(42)
-    image = np.random.rand(1, 3, 256, 256).astype(np.float32)
-    image = torch.from_numpy(image)
-
-    # Create the model
-    generator = Generator(in_channels=3)
-    discriminator = Discriminator(input_size=256*256, in_channels=3)
-
-    # Forward pass
-    output = generator(image)
-    print('Generator output shape:', output.shape)
-
-    output = discriminator(image)
-    print('Discriminator output shape:', output.shape)
-
-    # Loss function
-    criterion = nn.BCEWithLogitsLoss()
-
-    # Optimizers
-    generator_optimizer = torch.optim.Adam(generator.parameters(), lr=0.001, betas=(0.5, 0.999))
-    discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001, betas=(0.5, 0.999))
-
-    # Training loop
-    generator.train()
-    discriminator.train()
-    gen_losses = []
-    disc_losses = []
-
-    for epoch in range(10):
-        discriminator_optimizer.zero_grad()
-        generator_optimizer.zero_grad()
-
-        fake_image = generator(image)
-        real_disc = discriminator(image)
-        fake_disc = discriminator(fake_image)
-
-        # Discriminator
-        real_loss = criterion(real_disc, torch.ones_like(real_disc))
-        fake_loss = criterion(fake_disc, torch.zeros_like(fake_disc))
-
-        disc_loss = real_loss + fake_loss
-        disc_loss.backward()
-
-        discriminator_optimizer.step()
-
-        # Generator
-        fake_image = generator(image)
-        fake_disc = discriminator(fake_image)
-        gen_loss = criterion(fake_disc, torch.ones_like(fake_disc))
-        gen_loss.backward()
-
-        generator_optimizer.step()
-
-        gen_losses.append(gen_loss.item())
-        disc_losses.append(disc_loss.item())
-
-        if epoch % 10 == 0:
-            print(f'Epoch [{epoch}/{10}], Gen Loss: {gen_loss.item():.4f}, Disc Loss: {disc_loss.item():.4f}')
